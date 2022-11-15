4e1c8d33ffe1e90b68804e621581d05f077be34e
Turbotard <turbotard9@gmail.com>
2022-11-15 15:35:19 +0100

    meilleur database

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -3,8 +3,4 @@
 $user = "root";
 $pass = "root";
 $dbh = new PDO('mysql:host=localhost;dbname=puissance4', $user, $pass);
-$sth = $dbh->query('SELECT * FROM utilisateur');
-$donnees = $sth->fetchAll();
-print_r($donnees);
-echo 'eee';  
 ?>
