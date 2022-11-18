f4761232aee33e21488e0a537f6b24ac1943e4d1
matlam27 <matlam2712@gmail.com>
2022-11-18 14:31:03 +0100

    SCORE

--- score.php
+++ score.php
@@ -28,7 +28,7 @@ include "./view/header.inc.php";
                 <mark><strong> Pseudo </strong></mark>
                 <mark><strong> Niveau </strong></mark>
                 <mark><strong> Date et Heure </strong></mark>
-                <small><strong>score</strong></small>
+                <small class="score"><strong>score</strong></small>
               </li>
 
                 
@@ -36,48 +36,48 @@ include "./view/header.inc.php";
 
 
               <li class="lol">
-                <form>
+                <form class="truc">
                 <mark><strong> Kawoff </strong></mark>
                 <table><td><strong><a class="niveau"><strong>Difficile</strong></a> </strong></td>
-                  <td><a class="date"><strong>09/11/22</strong></a><a class="heure"><strong>10h23</strong></a></strong></td></table>
-                <small>22 s</small>
+                  <td><a class="date"><strong>09/11/22</strong></a><a class="date"><strong>10h23</strong></a></strong></td></table>
+                  <small class="nb"><strong>22 s</strong></small>
 </form>
               </li>
 
               <li class="lol">
-                <form>
+                <form class="truc">
                 <mark><strong> Turbo </strong></mark>
                 <table><td><strong><a class="niveau"><strong>Difficile</strong></a> </strong></td>
                   <td><strong><a class="date"><strong>23/10/22</strong></a><a class="date"><strong>03h46</strong> </a> </strong></td>
                 </table>
-                <small>34 s</small>
+                <small class="nb">34 s</small>
 </form>
               </li>
 
               <li class="lol">
-                <form>
+                <form class="truc">
                 <mark><strong> RedPointExe </strong></mark>
-                <table><td><strong><a class="niveau2"><strong>Moyen</strong></a> </strong></td>
+                <table><td><strong><a class="niveau"><strong>Moyen</strong></a> </strong></td>
                   <td><strong><a class="date2"><strong>11/04/22</strong></a><a class="date2"><strong>00h02</strong></a></strong></td></table>
-                <small>600 s</small>
+                  <small class="nb">600 s</small>
 </form>
               </li>
 
               <li class="lol">
-                <form>
-                <mark><strong>Matlam</strong></mark>
+                <form class="truc">
+                <markn class="machin"><strong>Matlam</strong></mark>
                 <table><td><strong><a class="niveau"><strong>Difficile</strong></a></strong></td>
-                  <td><strong><a class="date2"><strong>11/04/22</strong></a><a class="date2"><strong>08h02</strong></a></strong></td></table>
-                <small>602 s</small>
+                  <td><strong><a class="date"><strong>11/04/22</strong></a><a class="date"><strong>08h02</strong></a></strong></td></table>
+                  <small class="nb">602 s</small>
 </form>
               </li>
 
               <li class="lol">
-                <form>
+                <form class="truc">
                 <mark><strong> Naooki </strong></mark>
-                <table><td><strong><a class="niveau2"><strong>Facile</strong></a></strong></td>
+                <table><td><strong><a class="niveau"><strong>Facile</strong></a></strong></td>
                   <td><strong><a class="date3"><strong>29/02/22</strong></a><a class="date3"><strong>12h00</strong></a></strong></td></table>
-                <small>2600 s</small>
+                <small class="nb">2600 s</small>
 </form>
               </li>
 
