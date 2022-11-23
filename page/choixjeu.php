218bfe997f4171afb9e1d58c3132b60a054be3da
RedPointexe <benjaminhy04@gmail.com>
2022-11-23 10:36:30 +0100

    choixjeu

--- page/choixjeu.php
+++ page/choixjeu.php
@@ -1,5 +1,5 @@
 <?php 
-require("/Applications/MAMP/htdocs/Puissance4/view/header.inc.php");
+require("../view/header.inc.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,13 +9,13 @@ require("/Applications/MAMP/htdocs/Puissance4/view/header.inc.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Choix du jeu</title>
-    <link rel="stylesheet" href="choixjeu.css">
+    <link rel="stylesheet" href="../css/choixjeu.css">
     <script src="https://kit.fontawesome.com/81dc5c492f.js" crossorigin="anonymous"></script><script src="https://kit.fontawesome.com/81dc5c492f.js" crossorigin="anonymous"></script>
     <link href="https://www.dafontfree.net/embed/bXVsaS1zZW1pYm9sZCZkYXRhLzE2L20vNzg2NTYvTXVsaS1TZW1pQm9sZC50dGY" rel="stylesheet" type="text/css"/>
 </head>
 <body>
     <div id="fond">
-    <img id="back" src="fond-nuit-ville-futuriste-extraterrestre_1441-2823.jpg - copie - Petite.jpeg">
+    <img id="back" src="../image/fond-nuit-ville-futuriste-extraterrestre_1441-2823.jpg - copie - Petite.jpeg">
     <div id="opaque">
         <div id="boxe">
             <div id="entree">
@@ -44,10 +44,10 @@ require("/Applications/MAMP/htdocs/Puissance4/view/header.inc.php");
 
     
     <?php
-    require "/Applications/MAMP/htdocs/Puissance4/view/bouton.php";
+    require "../view/bouton.php";
     ?>
 </body>
 </html>
 <?php
-require "/Applications/MAMP/htdocs/Puissance4/view/footer.inc.php";
+require "../view/footer.inc.php";
 ?>
