ef5072cb7d5ae5b53a5a2cdffb381543001626d9
RedPointexe <benjaminhy04@gmail.com>
2022-11-25 09:33:43 +0100

    logo nasa

--- js/nasahuman.js
+++ js/nasahuman.js
@@ -33,7 +33,7 @@ function affichertab(){
         txt += "<div>";
         for (var j=0; j < tab[i].length;j++){
             if (tab[i][j] === 0){
-                txt += "<button class='btn btn-primary m-1' style='width: 150px;height:150px' onClick = 'verif(\""+i+"-"+j+"\")'><img style='width:150px;height:150px;padding:0%' src=../image/jeux/owerwatch/logooverwatch.png></button>";
+                txt += "<button class='btn btn-primary m-1' style='width: 150px;height:150px' onClick = 'verif(\""+i+"-"+j+"\")'><img style='width:150px;height:150px;padding:0%' src=../image/nasa/logonasa.jpg></button>";
             }else{
                 txt += "<img src ='"+getimage(tab[i][j])+"'  style='width: 150px;height:150px' class='m-1'>";
             }
